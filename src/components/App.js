8d04bc42f925846632c8115dd1e6aa52522f7aaa
Stadnyk Yura <pyrchuk@gmail.com>
2018-10-31 20:41:10 +0200

    remove unnecessary files

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ import "../styles/App.scss";
 import LandingPage from "./Landing";
 import HomePage from "./Home";
 import Navigation from "./Navigation";
-// import SignUpPage from './SignUp';
+
 import SignInPage from "../features/authorization/sign-in.page";
 import withAuthentication from "../features/authorization/with-authentication.hoc";
 
@@ -23,10 +23,6 @@ class App extends Component {
           <Route exact path={routes.LANDING} component={LandingPage} />
           <Route exact path={routes.SIGN_IN} component={SignInPage} />
           <Route exact path={routes.HOME} component={HomePage} />
-          {/* <Route
-            exact path={routes.SIGN_UP}
-            component={SignUpPage}
-          /> */}
         </div>
       </Router>
     );
