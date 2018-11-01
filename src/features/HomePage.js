17c2907094f37aa5eb36279c2f4ea2796034a1ba
Iryna Luk <41140625+IraLukianovets@users..gnoreplyithub.com>
2018-11-01 15:05:24 +0200

    added flat view to home page

--- src/features/HomePage.js
+++ src/features/HomePage.js
@@ -3,12 +3,14 @@ import React, { Component } from "react";
 import HomepageNav from "./homepage-nav-top/homepage-nav-top";
 // import "../styles/index.scss";
 import HomepageNavBottom from "./homepage-nav-bottom/homepage-nav-bottom";
+import FlatView from "./home-page/flat-view/flat-view";
 
 class HomePage extends Component {
   render() {
     return (
       <div className="App">
         <HomepageNav />
+        <FlatView />
         <HomepageNavBottom />
       </div>
     );
---
+++ src/features/home-page/flat-view/flat-view.js
@@ -0,0 +1,138 @@
+import React, { Component } from "react";
+import * as d3 from "d3";
+
+//import lightLogo from './light.svg';
+//import noLightLogo from './broken-light.svg';
+
+//import { firebase } from "../../../firebase";
+
+class FlatView extends Component {
+  constructor(props) {
+    super(props);
+    this.createFlatView = this.createFlatView.bind(this);
+
+    this.room = this.props.room;
+  }
+
+  componentDidMount() {
+    this.createFlatView();
+  }
+  componentDidUpdate() {
+    this.createFlatView();
+  }
+  createFlatView() {
+    var parent_div = d3.select("body").append("div");
+    parent_div.style("width", "80%");
+
+    var svg = parent_div
+      .append("svg")
+      .attr("viewBox", "0 0 700 500")
+      .attr("preserveAspectRatio", "xMidYMid")
+      .style("width", "80%");
+
+    svg.style("background-color", "rgba(0,0,0,0.08)");
+
+    svg
+      .append("rect")
+      .attr("width", 200)
+      .attr("height", 500)
+      .attr("x", 0)
+      .attr("y", 0)
+      .attr("fill", "rgba(33,66,255,0.4)")
+      .attr("stroke-width", 2)
+      .attr("stroke", "#000");
+
+    //var g = svg.append("g");
+    /*
+        var img = g.append("svg:image")
+            .attr("xlink:href", lightLogo)
+            .attr("width", 70)
+            .attr("height", 70)
+            .attr("x", 60)
+            .attr("y", 150);
+*/
+    //2
+    svg
+      .append("rect")
+      .attr("width", 200)
+      .attr("height", 250)
+      .attr("x", 300)
+      .attr("y", 0)
+      .attr("fill", "rgba(33,66,255,0.4)")
+      .attr("stroke-width", 2)
+      .attr("stroke", "#000");
+
+    //   var g = svg.append("g");
+    /*
+        var img = g.append("svg:image")
+            .attr("xlink:href", lightLogo)
+            .attr("width", 70)
+            .attr("height", 70)
+            .attr("x", 370)
+            .attr("y", 150);
+*/
+    /*   d3.selectAll("image")
+            .on("click", function () {
+                d3.select(this)
+                    .attr("xlink:href", noLightLogo);
+            });
+*/
+    // 3
+    svg
+      .append("rect")
+      .attr("width", 200)
+      .attr("height", 250)
+      .attr("x", 500)
+      .attr("y", 0)
+      .attr("fill", "rgba(33,66,255,0.4)")
+      .attr("stroke-width", 2)
+      .attr("stroke", "#000");
+
+    var text = svg
+      .append("text")
+      .attr("x", 100)
+      .attr("y", 70)
+      .attr("dy", "2em")
+      .attr("fill", "white");
+    // .text(this.room.name);
+
+    text
+      .style("font-family", "sans-serif")
+      .style("font-size", "24px")
+      .style("font-weight", 100)
+      .style("text-anchor", "middle");
+
+    //4
+    svg
+      .append("rect")
+      .attr("width", 200)
+      .attr("height", 200)
+      .attr("x", 500)
+      .attr("y", 300)
+      .attr("fill", "rgba(33,66,255,0.4)")
+      .attr("stroke-width", 2)
+      .attr("stroke", "#000");
+
+    // var g = svg.append("g");
+    /*
+        var img = g.append("svg:image")
+            .attr("xlink:href", lightLogo)
+            .attr("width", 70)
+            .attr("height", 70)
+            .attr("x", 60)
+            .attr("y", 150);
+            */
+  }
+  render() {
+    return (
+      <div>
+        <svg
+          ref={parent_div => (this.parent_div = parent_div)}
+          width={100}
+          height={200}
+        />
+      </div>
+    );
+  }
+}
+export default FlatView;
