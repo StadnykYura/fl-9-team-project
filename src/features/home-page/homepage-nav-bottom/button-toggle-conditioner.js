05ef3a576f66ea6d5a3db9b06fb2b00523aa0efa
Vitalii Namys <vitalik.namys@gmail.com>
2018-11-06 15:24:21 +0200

    fix warnings0; connect global action to firebase

--- src/features/home-page/homepage-nav-bottom/button-toggle-conditioner.js
+++ src/features/home-page/homepage-nav-bottom/button-toggle-conditioner.js
@@ -1,30 +1,66 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import { firebase } from '../../../firebase';
 
 class ToggleConditioner extends Component {
   constructor(props) {
     super(props);
-    this.state = { isToggleOn: false };
+    this.state = {
+      turnOnOffConditioner: true,
+    };
     this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick() {
-    this.setState(prevState => ({
-      isToggleOn: !prevState.isToggleOn
-    }));
-  }
+  handleClick = () => {
+    const user = firebase.auth.currentUser;
+    if (user) {
+      firebase.db
+        .collection('users')
+        .doc(user.uid)
+        .collection('rooms')
+        .get()
+        .then(documents => {
+          documents.docs.forEach(document => {
+            const roomDocRef = firebase.db
+              .collection('users')
+              .doc(user.uid)
+              .collection('rooms')
+              .doc(document.id);
+            if (document.data().consistConditioner) {
+              roomDocRef
+                .collection('devices')
+                .get()
+                .then(documents => {
+                  documents.docs.forEach(document => {
+                    const deviceDocRef = roomDocRef
+                      .collection('devices')
+                      .doc(document.id);
+                    deviceDocRef.update({
+                      isOn: this.state.turnOnOffConditioner,
+                    });
+                  });
+                });
+            }
+          });
+          this.setState(prevstate => ({
+            turnOnOffConditioner: !prevstate.turnOnOffConditioner,
+          }));
+        });
+    }
+  };
 
   render() {
+    const { turnOnOffConditioner } = this.state;
     return (
       <button
         onClick={this.handleClick}
         className={
-          this.state.isToggleOn
-            ? "menu__item_icon menu__conditioner turn_off"
-            : "menu__item_icon menu__conditioner turn_on"
+          turnOnOffConditioner
+            ? 'menu__item_icon menu__conditioner turn_on'
+            : 'menu__item_icon menu__conditioner turn_off'
         }
       >
-        <span>{this.state.isToggleOn ? "on" : "off"}</span>
-        <i class="switcher" />
+        <span>{turnOnOffConditioner ? 'on' : 'off'}</span>
+        <i className="switcher" />
       </button>
     );
   }
--- src/features/home-page/homepage-nav-bottom/button-toggle-light.js
+++ src/features/home-page/homepage-nav-bottom/button-toggle-light.js
@@ -1,11 +1,11 @@
-import React, { Component } from "react";
-import { firebase } from "../../../firebase";
+import React, { Component } from 'react';
+import { firebase } from '../../../firebase';
 
 class ToggleLight extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      turnOnOffLight: false
+      turnOnOffLight: true,
     };
     this.handleClick = this.handleClick.bind(this);
   }
@@ -15,43 +15,42 @@ class ToggleLight extends Component {
     const user = firebase.auth.currentUser;
     if (user) {
       firebase.db
-        .collection("users")
+        .collection('users')
         .doc(user.uid)
-        .collection("rooms")
+        .collection('rooms')
         .get()
         .then(documents => {
           documents.docs.forEach(document => {
             const roomDocRef = firebase.db
-              .collection("users")
+              .collection('users')
               .doc(user.uid)
-              .collection("rooms")
+              .collection('rooms')
               .doc(document.id);
             batch.update(roomDocRef, {
-              turnOnOffLight: this.state.turnOnOffLight
+              turnOnOffLight: !this.state.turnOnOffLight,
             });
           });
           batch.commit();
-          this.setState({
-            turnOnOffLight: !this.state.turnOnOffLight
-          });
+          this.setState(prevstate => ({
+            turnOnOffLight: !prevstate.turnOnOffLight,
+          }));
         });
-    } else {
-      console.log("User didn`t sign in");
     }
   };
 
   render() {
+    const { turnOnOffLight } = this.state;
     return (
       <button
         onClick={this.handleClick}
         className={
-          this.state.turnOnOffLight
-            ? "menu__item_icon menu__light turn_off"
-            : "menu__item_icon menu__light turn_on"
+          turnOnOffLight
+            ? 'menu__item_icon menu__conditioner turn_on'
+            : 'menu__item_icon menu__conditioner turn_off'
         }
       >
-        <span>{this.state.turnOnOffLight ? "on" : "off"}</span>
-        <i class="switcher" />
+        <span>{turnOnOffLight ? 'on' : 'off'}</span>
+        <i className="switcher" />
       </button>
     );
   }
