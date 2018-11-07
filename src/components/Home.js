e822b9809738404ec27179fcbc086e29cb459db6
Iryna Luk <41140625+IraLukianovets@users..gnoreplyithub.com>
2018-11-07 19:40:45 +0200

    deleted whitespace

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,7 @@ import withAuthorization from "../features/authorization/with-authorization.hoc"
 import HomepageNavTop from "../features/home-page/homepage-nav-top/homepage-nav-top";
 import HomepageNavBottom from "../features/home-page/homepage-nav-bottom/homepage-nav-bottom";
 import FlatView from "../features/home-page/flat-view/flat-view";
+
 function Home() {
   return (
     <div>
