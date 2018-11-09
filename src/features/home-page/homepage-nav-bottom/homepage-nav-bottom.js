45b9cad2d6afe2496cc772445d5835852981aa8f
basystka <basystka@gmail.com>
2018-11-09 18:48:26 +0200

    move day/night button

--- src/features/home-page/homepage-nav-bottom/homepage-nav-bottom.js
+++ src/features/home-page/homepage-nav-bottom/homepage-nav-bottom.js
@@ -13,9 +13,6 @@ class HomepageNavBottom extends Component {
           <li className="menu__item menu__item-nobg">
             <ToggleConditioner />
           </li>
-          <li className="menu__item">
-            <ToggleDay />
-          </li>
           <li className="menu__item menu__item-nobg">
             <span className="menu__weather">Weather</span>
           </li>
@@ -26,6 +23,9 @@ class HomepageNavBottom extends Component {
             <a className="menu__link" href="?">
               Global action
             </a>
+          </li>
+          <li className="menu__item">
+            <ToggleDay />
           </li>
         </ul>
       </section>
