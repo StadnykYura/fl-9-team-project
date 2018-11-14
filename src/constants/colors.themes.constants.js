fe4692694468f88df520427f91c077f38ea740a7
basystka <basystka@gmail.com>
2018-11-14 15:04:01 +0200

    create theme constants

---
+++ src/constants/colors.themes.constants.js
@@ -0,0 +1,29 @@
+const themes = {
+  LIGHT: {
+    zero: '#fff',
+    primaryBG: '#f3f4f8',
+    link: '#455eee',
+    linkBG: '#fff',
+    hoverBG: '#112abc',
+    active: '#fff',
+    activeBG: '#0d208d',
+    dark: '#000',
+    error: '#ff0000',
+    'zero-rgb': '255, 255, 255',
+    'hover-link': '#fff',
+    'menu-border': '#e9eaee',
+    'menu-color': '#797a8c',
+  },
+  DARK: {
+    zero: '#727ead',
+    primaryBG: '#929bc0',
+    link: '#fff',
+    linkBG: '#727ead',
+    hoverBG: 'transparent',
+    active: '#fff',
+    error: '#ff0000',
+    'hover-link': '#fff',
+  },
+};
+
+export default themes;
