b58a5a627a1b75e3b27bf2591070a9edb719ac5e
basystka <basystka@gmail.com>
2018-11-19 13:38:46 +0200

    change weekday - short view

--- src/features/home-page/date-time/show-date.js
+++ src/features/home-page/date-time/show-date.js
@@ -7,7 +7,7 @@ class ShowDate extends Component {
       date: new Date().toLocaleString('en-En', {
         day: 'numeric',
         month: 'short',
-        weekday: 'long',
+        weekday: 'short',
       }),
     };
   }
