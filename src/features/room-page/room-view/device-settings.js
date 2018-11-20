35baf050525b1e4b1abc90a967aaa4111e34ce0a
Stadnyk Yura <pyrchuk@gmail.com>
2018-11-20 13:57:24 +0200

    more styles to room-view

--- src/features/room-page/room-view/device-settings.js
+++ src/features/room-page/room-view/device-settings.js
@@ -29,7 +29,7 @@ export default class DeviceSettings extends Component {
   }
 
   render() {
-    console.log(this.props);
+    // console.log(this.props);
     const { currentDevice } = this.props;
 
     // if (this.props.currentDevice.deviceSettings) {
