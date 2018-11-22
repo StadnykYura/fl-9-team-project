9cfb305ca19676117103858bbbf1dc2c6623df1e
Stadnyk Yura <pyrchuk@gmail.com>
2018-11-22 14:37:47 +0200

    add error handling to root component

---
+++ src/components/erorr-boundary.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+export default class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { error: null, errorInfo: null };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    this.setState({
+      error: error,
+      errorInfo: errorInfo,
+    });
+  }
+
+  render() {
+    if (this.state.errorInfo) {
+      return (
+        <div>
+          <h2>Something went wrong. Please reload the page</h2>
+        </div>
+      );
+    }
+
+    return this.props.children;
+  }
+}
